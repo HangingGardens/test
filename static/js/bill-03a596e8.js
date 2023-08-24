79a88989bb0b0891a5737d5a582b3d6a9ea494eb
wkeai <wkeai8888@gmail.com>
2023-08-24 17:34:21 +0800

    re-build

---
+++ static/js/bill-03a596e8.js
@@ -0,0 +1,1 @@
+import{U as a}from"./index-005617de.js";const t=s=>a("GET","/bills/getBillDetails",s),l=s=>a("POST","/bills/scanPay",s),e=s=>a("POST","/bills/pay",s);export{e as a,t as b,l as q};
--- static/js/bill-6c6d7441.js
+++
@@ -1,1 +0,0 @@
-import{U as a}from"./index-968154d2.js";const t=s=>a("GET","/bills/getBillDetails",s),l=s=>a("POST","/bills/scanPay",s),e=s=>a("POST","/bills/pay",s);export{e as a,t as b,l as q};
